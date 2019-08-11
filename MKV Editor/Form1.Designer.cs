b4ca10e2556328f92818bbb33a91b4e7d206b92f
kamin0519 <blue52990122@gmail.com>
2019-08-12 00:34:50 +0900

    新規追加 v1.0.0.0

---
+++ MKV Editor/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace MKV_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "mkvmerge.exe";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(259, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(367, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "開く";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "mkvがあるフォルダ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(259, 19);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(367, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "開く";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(367, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "開く";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(102, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(259, 19);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "出力先のフォルダ";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(226, 126);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(100, 16);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "字幕を削除する";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(332, 126);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(120, 16);
+            this.checkBox4.TabIndex = 12;
+            this.checkBox4.Text = "チャプターを削除する";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(440, 67);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "実行";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Checked = true;
+            this.checkBox5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox5.Location = new System.Drawing.Point(108, 68);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(130, 16);
+            this.checkBox5.TabIndex = 14;
+            this.checkBox5.Text = "サブフォルダも参照する";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(120, 126);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(100, 16);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "音声を削除する";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 126);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 16);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "映像を削除する";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 233);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MKV Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ MKV Editor/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MKV_Editor
+{
+    public partial class Form1 : Form
+    {
+        private string mkvmerge_fileName = "";
+        private string input_folder = "";
+        private string output_folder = "";
+        private List<string> input_folderFiles = new List<string>();
+        private bool IsSomeFolder = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "mkvmerge.exeを選択してください。";
+            openFileDialog1.FileName = "mkvmerge.exe";
+            openFileDialog1.Filter = "mkvmerge.exe|mkvmerge.exe";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default.mkvmerge = mkvmerge_fileName = textBox1.Text = openFileDialog1.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Description = "mkvがあるフォルダを指定してください。";
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Desktop;
+            if (folderBrowserDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                input_folderFiles.Clear();
+                input_folder = textBox2.Text = folderBrowserDialog1.SelectedPath;
+                IEnumerable<string> files;
+                if (checkBox5.Checked)
+                    files = Directory.EnumerateFiles(folderBrowserDialog1.SelectedPath, "*", SearchOption.AllDirectories);
+                else
+                    files = Directory.EnumerateFiles(folderBrowserDialog1.SelectedPath, "*", SearchOption.TopDirectoryOnly);
+                foreach (string f in files)
+                    if (Path.GetExtension(f) == ".mkv")
+                        input_folderFiles.Add(Path.GetFileName(f));
+                if (input_folderFiles.Count == 0)
+                {
+                    MessageBox.Show("mkvがあるフォルダを指定してください", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    button4.Enabled = false;
+                }
+                else
+                {
+                    button4.Enabled = true;
+                }
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Description = "出力先フォルダを指定してください。";
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Desktop;
+            if (folderBrowserDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                if (input_folder == output_folder)
+                {
+                    if (MessageBox.Show("mkvがあるフォルダと同じフォルダですが、よろしいですか？\r\n\r\n(ファイル名)_2.mkvといった感じになります。", "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+                    {
+                        IsSomeFolder = true;
+                        output_folder = textBox3.Text = folderBrowserDialog1.SelectedPath;
+                    }
+                    else MessageBox.Show("キャンセルされました。", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    output_folder = textBox3.Text = folderBrowserDialog1.SelectedPath;
+                    IsSomeFolder = false;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(Properties.Settings.Default.mkvmerge))
+                mkvmerge_fileName = textBox1.Text = Properties.Settings.Default.mkvmerge;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(mkvmerge_fileName)) { MessageBox.Show("mkvmerge.exeを指定してください", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+            if (string.IsNullOrWhiteSpace(input_folder)) { MessageBox.Show("mkvがあるフォルダを指定してください", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+            if (string.IsNullOrWhiteSpace(output_folder)) { MessageBox.Show("出力先フォルダを指定してください", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+
+            bool Isrun = true;
+            if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked && checkBox4.Checked)
+                if (MessageBox.Show("何もないファイルが生成されますが、よろしいですか？", "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.No)
+                    Isrun = false;
+
+            if (Isrun)
+            {
+                foreach (string file in input_folderFiles)
+                {
+                    string _input = input_folder + "\\" + file;
+                    string _file = file;
+                    if (IsSomeFolder) _file = Path.GetFileNameWithoutExtension(_file) + "_2" + Path.GetExtension(_file);
+                    string _output = output_folder + "\\" + _file;
+                    string command = "--output \"" + _output + "\"";
+                    if (checkBox1.Checked && !checkBox2.Checked)
+                        command = "--output \"" + _output.Replace(".mkv", ".mka") + "\"";
+                    if (checkBox1.Checked && checkBox2.Checked && (!checkBox3.Checked | !checkBox4.Checked))
+                        command = "--output \"" + _output.Replace(".mkv", ".mks") + "\"";
+                    if (checkBox1.Checked) command += " --no-video";
+                    if (checkBox2.Checked) command += " --no-audio";
+                    if (checkBox3.Checked) command += " --no-subtitles";
+                    if (checkBox4.Checked) command += " --no-chapters";
+                    command += " \"" + _input + "\"";
+                    Process process = Process.Start(mkvmerge_fileName, command);
+                    process.WaitForExit();
+                }
+                MessageBox.Show("終了しました。", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else MessageBox.Show("キャンセルされました。", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
